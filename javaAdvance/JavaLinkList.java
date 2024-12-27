c34e9a55954b8e658fac55bd17f25cc2e653885b
Daniel Peter <peterdanielfun24@gmail.com>
2024-12-27 16:01:43 +0100

    getting better

---
+++ javaAdvance/JavaLinkList.java
@@ -0,0 +1,22 @@
+package javaAdvance;
+
+import java.util.LinkedList;
+
+public class JavaLinkList {
+    public static void main(String[] args){
+        LinkedList<String>linkedList = new LinkedList<>();
+
+        linkedList.add("Apple");
+        linkedList.add("orange");
+        linkedList.add("pearl");
+
+        linkedList.addFirst("mango");
+
+        linkedList.remove("orange");
+        linkedList.removeFirst();
+        linkedList.removeLast();
+        linkedList.clear();
+
+        System.out.println(linkedList);
+    }
+}
--- javaAdvance/shoppingList.java
+++ javaAdvance/shoppingList.java
@@ -6,11 +6,19 @@ public class shoppingList {
     linkedList items;
 
     public shoppingList(){
-        this.items = new linkedList(0);
+        this.items = new linkedList(null);
     }
 
-    public void add(){
-        
+    public void addItem(String itemName){
+        this.items.append(itemName);
+    }
+
+    public void removeItem(String itemName){
+        this.items.remove(itemName);
+    }
+
+    public void printItem(){
+        this.items.printList();
     }
     public static void main(String[] args){
 
