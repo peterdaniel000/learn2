f92f093f4e819e12c634bbf0ceb4c3a686a3bef5
Daniel Peter <peterdanielfun24@gmail.com>
2024-12-30 15:45:31 +0100

    keep going

---
+++ javaAdvance/kidsGreatestCandies.java
@@ -0,0 +1,18 @@
+package javaAdvance;
+
+public class kidsGreatestCandies {
+    /*
+     * public list<Boolean> kidsWithCandies(int[] candies, int extraCandies){
+     *          int maxCandies = 0;
+     *          List<Boolean> result = new ArrayList<>();
+     * 
+     *          for(int candy: candies){
+     *                 maxCandies = Math.max(maxCandies, candy);
+     *             }
+     *          for(int candy: candies){
+     *                  result.add(candy + extraCandies => maxCandies);
+     *             }
+     *          return result,
+     *      }
+     */
+}
