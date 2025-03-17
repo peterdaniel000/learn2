2109ae794f07c5ee12f8a4f13bef55324202ddfb
Daniel Peter <peterdanielfun24@gmail.com>
2025-03-17 02:03:52 +0100

    keep going

---
+++ queue/minimumDepth.java
@@ -0,0 +1,21 @@
+package queue;
+
+public class minimumDepth {
+    /*
+     *
+    private int minDepthHelper(TreeNode node){
+        if (node == null) return 0;
+        
+        if (node.left == null){
+            return 1 + minDepthHelper(node.left);
+        } else if (node.right == null){
+            return 1 + minDepthHelper(node.right) 
+        }
+
+        return 1 + Math.min(minDepthHelper(node.right), minDepthHelper(node.left));
+        }
+     public int minDepth(TreeNode){
+         return minDepthHelper(root);
+        }
+     */
+}
---
+++ queue/sumOfLeftLeavesNode.java
@@ -0,0 +1,23 @@
+package queue;
+
+public class sumOfLeftLeavesNode {
+    /*
+      private int helper(TreeNode root, boolean isLeft) {
+        if (node == null) return 0;
+        if (node.left == null && node.right == null){
+            if (isLeft) {
+                return node.val
+            } else {
+                return 0; 
+            }
+        }
+        int res = 0;
+        res += helper(node.left, true);
+        res += helper(node.right, false);
+        return res;
+      }
+      public int sumOfLeftLeavesNode(TreeNode root){
+      return helper(root, false);
+      }
+     */
+}
