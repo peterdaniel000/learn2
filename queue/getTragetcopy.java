590f38b7ac62df74b024cb8c8b6c07aef260d8a0
Daniel Peter <peterdanielfun24@gmail.com>
2025-03-17 02:29:39 +0100

    keep going

---
+++ queue/getTragetcopy.java
@@ -0,0 +1,19 @@
+package queue;
+
+public class getTragetcopy {
+    /*
+    private TreeNode dfs(final TreeNode nodeOriginal, final TreeNode nodeCopy, final TreeNode target){
+        if (nodeOriginal == null) return null;
+        if (nodeOriginal == target) return nodeCopy;
+
+        TreeNode left = dfs(nodeOrinal.left,nodeCopy.left, target);
+         TreeNode left = dfs(nodeOrinal.right,nodeCopy.right, target);
+
+         return left == null ? right : left;
+
+        }
+     public final TreeNode getTargetCopy(final TreeNode original, final TreeNode cloned, final TreeNode target) {
+     return dfs(Original,copy, target);
+     }
+     */
+}
