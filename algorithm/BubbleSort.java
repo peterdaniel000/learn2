747fe4d5fa1849eeb00bfd11d605b7e648db7949
Isaiah peter <peterisaiah4fun@gmail.com>
2025-07-21 14:48:32 +0100

    keep going

--- algorithm/BubbleSort.java
+++ algorithm/BubbleSort.java
@@ -28,7 +28,7 @@ public class BubbleSort{
 
     public static void printArray(int[] arr){
         for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i] + " ");
+            System.out.print(arr[i] + " ");
         }
         System.out.println();
     }
