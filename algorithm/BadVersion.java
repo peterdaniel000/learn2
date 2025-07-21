7ad2c8c7d7450d6d74e8f15f3e7408ab8712f4b7
Isaiah peter <peterisaiah4fun@gmail.com>
2025-07-21 14:47:37 +0100

    keep going

---
+++ algorithm/BadVersion.java
@@ -0,0 +1,23 @@
+package algorithm;
+
+public class BadVersion {
+    /*
+     * public class solution extends VersionControl{
+     * public int firstBadVersion(int n) {
+     * int left = 0;
+     * int right = n;
+     * while (left < right) {
+     *  int mid = left + (right - left) / 2;
+     * 
+     *  if (BadVersion(mid)) {
+     * right = mid;
+     * } else {
+     * left = mid + 1
+     * }
+     * }
+     * return left;
+     * }
+     * }
+     */
+    
+}
